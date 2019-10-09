4f3166230e8dd4d2700031e5e68521415afd727a
akimaleo <akimaleo@ya.ru>
2019-10-09 21:48:13 +0300

    Information about oblast in separate windows on click was integrated in app gradle update

--- app/src/main/java/com/letit0or1/kawa/sum1/arukraine/ui/activity/MainActivity.kt
+++ app/src/main/java/com/letit0or1/kawa/sum1/arukraine/ui/activity/MainActivity.kt
@@ -10,47 +10,52 @@ import com.google.ar.core.Frame
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
-import com.google.ar.sceneform.rendering.Color
-import com.google.ar.sceneform.rendering.MaterialFactory
-import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.ShapeFactory
+import com.google.ar.sceneform.rendering.*
 import com.letit0or1.kawa.sum1.arukraine.ui.SnackbarHelper
 import com.letit0or1.kawa.sum1.arukraine.ui.fragment.ARUkraineFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import com.letit0or1.kawa.sum1.arukraine.R
 import com.letit0or1.kawa.sum1.arukraine.data.AugmentedImageNode
+import com.letit0or1.kawa.sum1.arukraine.data.Oblast
+import kotlinx.android.synthetic.main.oblast_view_info.view.*
 import java.util.HashMap
-import java.util.concurrent.locks.Lock
+import java.util.concurrent.CompletableFuture
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SELECTED_NODE_Y_POS: Float = 0.02f
+        private const val STANDART_NODE_Y_POS: Float = 0.00f
+    }
 
     val viewModel: MainViewModel by viewModel()
 
     private lateinit var fitToScanView: ImageView
     private lateinit var arFragment: ARUkraineFragment
-    private lateinit var redSphereRenderable: ModelRenderable
     // Augmented image and its associated center pose anchor, keyed by the augmented image in
     // the database.
     private val augmentedImageMap = HashMap<AugmentedImage, AugmentedImageNode>()
-
+    private var solarControlsRenderable: ViewRenderable? = null
+    lateinit var solarControlsStage: CompletableFuture<ViewRenderable>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        solarControlsStage =
+            ViewRenderable.builder().setView(this, R.layout.oblast_view_info).build()
+
         fitToScanView = fit_to_scan_view
         arFragment = ux_fragment as ARUkraineFragment
-        arFragment.arSceneView.scene.addOnUpdateListener { onUpdateFrame(it, arFragment.arSceneView.arFrame) }
+        arFragment.arSceneView.scene.addOnUpdateListener {
+            onUpdateFrame(it, arFragment.arSceneView.arFrame)
+        }
 
-        // Create a new anchor for newly found images.
-        MaterialFactory.makeOpaqueWithColor(this, Color(getColor(android.R.color.background_dark)))
-            .thenAccept { material ->
-                redSphereRenderable =
-                    ShapeFactory.makeSphere(0.5f, Vector3(0.0f, 0.0f, 0.0f), material)
-            }
-
+        solarControlsStage.thenAccept { solarControlsRenderable = it }
     }
 
     private fun onUpdateFrame(frameTime: FrameTime, frame: Frame?) {
@@ -71,11 +76,9 @@ class MainActivity : AppCompatActivity() {
                     // Create a new anchor for newly found images.
                     if (!augmentedImageMap.containsKey(augmentedImage)) {
                         val node = AugmentedImageNode(this, arFragment.transformationSystem)
+
                         node.onClickListener = { _, _, node, oblast ->
-                            data.text = getString(oblast.stringRes)
-                            data.text = data.text.toString() + "\n${node.localPosition}"
-//                            xV.text = "x: $value"
-                            attach(node)
+                            attach(node, oblast)
                         }
                         node.setImage(augmentedImage)
                         augmentedImageMap[augmentedImage] = node
@@ -90,14 +93,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun attach(node: Node) {
-        val divideOn = 400;
+    private var selectedNode: Node? = null
+    private lateinit var uiNode: Node
+
+    private fun attach(node: Node, oblast: Oblast) {
+        selectedNode?.let {
+            val pos = it.localPosition
+            pos.y = STANDART_NODE_Y_POS
+            it.localPosition = pos
+        }
+        selectedNode = node
+        data.text = getString(oblast.stringRes)
+        data.text = data.text.toString() + "\n${node.localPosition}"
+
+        val curPos = node.localPosition
+        curPos.y = SELECTED_NODE_Y_POS
+        node.localPosition = curPos
+        applyInfoToView(node, oblast)
+    }
+
+    private fun applyInfoToView(node: Node, oblast: Oblast) {
+        if (!::uiNode.isInitialized) {
+            uiNode = Node()
+            uiNode.localScale = Vector3(0.1f, 0.1f, 0.1f)
+            uiNode.localRotation = Quaternion(Vector3(1f, 0f, 0f), -75f)
+        }
+        uiNode.renderable = solarControlsRenderable
+        uiNode.localPosition = node.localPosition
+        uiNode.localScale = Vector3(0.1f, 0.1f, 0.1f)
+
+        val solarControlsView = solarControlsRenderable!!.view
+        solarControlsView.obl_info_title.text = viewModel.getDataForOblast(oblast, this)
+        solarControlsView.closeView.setOnClickListener {
+            uiNode.setParent(null)
+        }
+
+        uiNode.setParent(node)
+    }
+
+    private fun debugAttach(node: Node, oblast: Oblast) {
+
+        val divideOn = 400
         fun valueFromSeekBar(seekBar: SeekBar): Float {
             return (seekBar.progress - 50).toFloat() / divideOn
         }
 
         fun setSeekBarFromValue(seekBar: SeekBar, value: Float) {
-            seekBar.progress = (((value) + 50 / divideOn) * divideOn).toInt()+50
+            seekBar.progress = (((value) + 50 / divideOn) * divideOn).toInt() + 50
         }
 
         x.setOnSeekBarChangeListener(null)
@@ -113,15 +155,18 @@ class MainActivity : AppCompatActivity() {
                 when (seekBar) {
                     x -> {
                         xV.text = "x: ${valueFromSeekBar(x)}"
-                        node.localPosition = Vector3(valueFromSeekBar(x), node.localPosition.y, node.localPosition.z)
+                        node.localPosition =
+                            Vector3(valueFromSeekBar(x), node.localPosition.y, node.localPosition.z)
                     }
                     y -> {
                         yV.text = "y: ${valueFromSeekBar(y)}"
-                        node.localPosition = Vector3(node.localPosition.x, valueFromSeekBar(y), node.localPosition.z)
+                        node.localPosition =
+                            Vector3(node.localPosition.x, valueFromSeekBar(y), node.localPosition.z)
                     }
                     z -> {
                         zV.text = "z: ${valueFromSeekBar(z)}"
-                        node.localPosition = Vector3(node.localPosition.x, node.localPosition.y, valueFromSeekBar(z))
+                        node.localPosition =
+                            Vector3(node.localPosition.x, node.localPosition.y, valueFromSeekBar(z))
                     }
                 }
             }
--- app/src/main/java/com/letit0or1/kawa/sum1/arukraine/ui/activity/MainViewModel.kt
+++ app/src/main/java/com/letit0or1/kawa/sum1/arukraine/ui/activity/MainViewModel.kt
@@ -1,7 +1,13 @@
 package com.letit0or1.kawa.sum1.arukraine.ui.activity
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.letit0or1.kawa.sum1.arukraine.data.Oblast
 
 class MainViewModel : ViewModel() {
 
+
+    fun getDataForOblast(oblast: Oblast, context: Context): String {
+        return context.getString(oblast.descriptionRes)
+    }
 }
